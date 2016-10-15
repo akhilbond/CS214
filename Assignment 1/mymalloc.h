cd5e01acb11ba45fbb158e19f97680b6300a6787
vb239 <vamshibala@yahoo.com>
2016-10-14 23:09:49 -0400

    Update mymalloc.h

--- Assignment 1/mymalloc.h
+++ Assignment 1/mymalloc.h
@@ -1,20 +1,9 @@
-#ifndef MALLOC_H
-#define MALLOC_H
+#ifndef _MALLOC
+#define _MALLOC
+#define MEM_SIZE 5000
 
-#ifndef NULL
-#define NULL 0
-#endif
-
-#define malloc(x) myMalloc(x,__FILE__,__LINE__);
-#define free(x) myFree(x,__FILE__,__LINE__);
-
-/*
- *  My malloc implementation
- */
-void* myMalloc(unsigned , char*, int);
-/*
- *  My free implementation
- */
-void myFree(void*, char*, int);
+void* mymalloc(int memory);
+void myfree(void*);
+void mycheck();
 
 #endif

412d522be7f5fa54da11bd721a595fdbe2eca527
vb239 <vamshibala@yahoo.com>
2016-10-14 23:08:40 -0400

    Update mymalloc.c

--- Assignment 1/mymalloc.c
+++ Assignment 1/mymalloc.c
@@ -1,107 +1,261 @@
-#include "mymalloc.h"
 #include <stdio.h>
-#define MEMSIZE 5000
-#define ENTRY_SIZE sizeof( MemEntry )
+#include "mymalloc.h"
+#define SECURE 3343  //security code
 
-// Creating boolean data type
-typedef enum {false =0, true} bool;
 
-typedef enum {
-        MALLOC_INSUFFICIENT,
-        FREE_REDUNDANT,
-        FREE_NOT_ALLOCATED,
-        FREE_MIDDLE
-} Error;
+typedef struct LL{   		//creates a doubly linked list the metadata linked list
 
-static char mem[MEMSIZE] = {'0'};
+	struct LL* next;		//points to the next node in the linked list
+	struct LL* prev;		//points to the previous node in the liked list
+	unsigned int allocated:1;	//variable to determine if memory is allocated or not
+	int size; 					//size of the memory of that node
+	unsigned int secure:16;		//security code for that node
 
-typedef struct MemEntry MemEntry;
-typedef struct MemEntry* MemEntryPtr;
+} Node;
 
-struct MemEntry {
-        //Previous
-        MemEntryPtr prev;
+static int firstmalloccalled = 0;	//determines if malloc is called or not
+static char myblock[MEM_SIZE];	//creates an array of size 5000
+static Node* head = (Node*)(&(myblock[0]));
 
-        //Next
-        MemEntryPtr next;
 
-        //Status of the block of memory
-        bool isFree;
 
-        // fileFree indicates the file containing the free command which frees the entry
-        char *fileFree;
+void* mymalloc(int memory){
+	
+	if(memory > MEM_SIZE - sizeof(Node)){
+	
+		printf("Error is calling mymalloc \n");
+		return NULL;
+	
+	} 
+	
+	int donemalloc = 0;
+	
+	if(!firstmalloccalled){
+	
+		head->size = MEM_SIZE - sizeof(Node);
+		head->next = NULL;
+		head->prev = NULL;
+		head->allocated = 0;
+		head->secure = SECURE;
+		firstmalloccalled = 1;
+	
+	}
+	
+	int index = 0;
 
-        // lineFree indicates the line containing the free command which frees the entry
-        int lineFree;
-        // Available size for or the size of the data
-        // When isFree-d, it stores the available size for allocation
-        // Otherwise, size is the size of memory allocated
-        unsigned size;
-};
+	Node *ptr = head;
+	while(ptr != NULL){
+		
+		if(ptr->size >= memory && ptr->allocated == 0){
+		
+			break;
+			
+		}
+		index = index + sizeof(Node) + ptr->size;
+		ptr = ptr->next;
+		
+	}
+		
+	int temp = ptr->size;
+	ptr->size = memory;
+	ptr->allocated = 1;	
+			
+	if(ptr->next == NULL){
+		
+		if(temp - memory > sizeof(Node)){
+			
+			ptr->next = (Node*)(&(myblock[index + sizeof(Node) + memory]));
+			ptr->next->allocated = 0;
+			ptr->secure = SECURE;
+			ptr->next->size = temp - sizeof(Node) - memory;
+			ptr->next->next = NULL;
+			ptr->next->prev = ptr;
+							
+		}else{
+		
+			ptr->size = ptr->size + (temp - memory);
+			
+		}
+		
+		donemalloc = 1;
+		
+					
+	}else{
+		
+		if(temp - memory > sizeof(Node)){
+		
+			Node* temporaryNode = ptr->next;
+			ptr->next = (Node*)(&(myblock[index + sizeof(Node) + memory]));
+			ptr->secure = SECURE;
+			ptr->next->allocated = 0;
+			ptr->next->size = temp - sizeof(Node) - memory;
+			ptr->next->next = temporaryNode;
+			ptr->next->prev = ptr;
+			temporaryNode->prev = ptr->next;
+				
+		}else{
+		
+			ptr->size = ptr->size + (temp - memory);
+		
+		}
+		
+		donemalloc = 1;	
+		
+			
+	}
+	
+	
+	if(donemalloc == 0){
+	
+		printf("Error in calling mymalloc");
+		return NULL;
+	
+	}
+	
+	
+	
+	
+	
+	
+	return (char*)ptr + sizeof(Node);
 
-// shows whether the head is made or not
-static bool initialized = false;
-// head pointer
-static MemEntryPtr head;
-
-void errorReport(char* file, int caller_line, Error error, void* address){
-        fprintf(stderr, "%s:%d: error: ", file, caller_line);
-        switch (error) {
-        case MALLOC_INSUFFICIENT:
-                fprintf(stderr,"Memory is insufficient to malloc().");
-                break;
-        case FREE_REDUNDANT:
-                fprintf(stderr,"Memory @%p is already free-d.", address);
-                break;
-        case FREE_NOT_ALLOCATED:
-                fprintf(stderr,"Memory @%p is not pre-allocated.",address);
-                break;
-        case FREE_MIDDLE:
-                fprintf(stderr,"Memory @%p is not the previously returned address by malloc().",address);
-                break;
-        }
-        printf("\n");
 }
 
-void* myMalloc(unsigned size, char* file, int caller_line){
-        if(!initialized) {
-                //initialize the head
-                head = (MemEntryPtr)mem;
-                head->prev = NULL;
-                head->next = NULL;
-                head->isFree = true;
-                head->fileFree = NULL;
-                head->lineFree = 0;
-                head->size = MEMSIZE - ENTRY_SIZE;
-                initialized = true;
-        }
 
-        MemEntryPtr temp = head;
 
-        //While the tracking node is not null
-        while (temp != NULL) {
-                //the node is freed
-                if (temp->isFree && temp->size >= size + ENTRY_SIZE) {
-                        MemEntryPtr newEntry = (MemEntryPtr)( ((char)temp) + ENTRY_SIZE + size);
-                        newEntry->prev = temp;
-                        newEntry->next = temp->next;
-                        temp->next = newEntry;
-                        if (newEntry->next!=NULL) {
-                                newEntry->next->prev = newEntry;
-                        }
 
-                        newEntry->isFree = true;
 
-                        //Adjust current available size of entry
-                        newEntry->size = temp->size - size -ENTRY_SIZE;
 
-                        temp->isFree = false;
-                        temp->size = size;
-                        return (void*) ((char*)temp)+ENTRY_SIZE;
-                }
-                temp = temp->next;
-        }
-        // Not enough space to malloc
-        errorReport(file,caller_line, MALLOC_INSUFFICIENT, 0);
-        return NULL;
+
+void myfree(void* input){
+	
+	
+	Node* ptr = (Node*)(input - sizeof(Node));
+	
+	if(ptr->secure != SECURE || ptr->allocated == 0){
+
+		printf("ERROR: cannot free pointer \n");
+		return;
+		
+	}
+	
+	
+	if(ptr->next != NULL && ptr->prev == NULL){
+	
+		Node* temp = ptr->next;
+		
+		if(temp->allocated == 0){
+		
+			ptr->size = ptr->next->size + ptr->size + sizeof(Node);
+			ptr->allocated = 0;
+			ptr->next = ptr->next->next;
+			temp->prev = NULL;
+			
+			if(ptr->next != NULL){
+			
+				ptr->next->prev = ptr;
+			
+			}
+			
+		}else{
+		
+			ptr->allocated = 0;
+		
+		}
+	
+	}else if(ptr->next == NULL && ptr->prev != NULL){
+	
+		Node* temp = ptr->prev;
+		
+		if(temp->allocated == 0){
+			
+			if(temp->prev == NULL){
+			
+				temp->size = temp->size + ptr->size + sizeof(Node);
+				temp->next = ptr->next;
+				ptr->prev = ptr;
+				
+				if(temp->next != NULL){
+				
+					temp->next->prev = temp;
+				
+				}
+			
+			}else{
+			
+				temp->size = temp->size + sizeof(Node) + ptr->size;
+				temp->next = NULL;
+			
+			}
+		
+		}else{
+		
+			ptr->allocated = 0;
+		
+		}
+	
+	}else if(ptr->next != NULL && ptr->prev != NULL){
+	
+		if(ptr->next->allocated == 0 && ptr->prev->allocated == 0){
+		
+			ptr = ptr->prev;
+			ptr->size = ptr->size + sizeof(Node) + ptr->next->size + sizeof(Node) + ptr->next->next->size;
+			
+			
+			if(ptr->next->next->next != NULL){
+			
+				ptr->next = ptr->next->next->next;
+				ptr->next->next->next->prev = ptr;
+				
+			}else{
+			
+				ptr->next = NULL;
+			
+			}
+		
+		}else if(ptr->prev->allocated != 0 && ptr->next->allocated == 0){
+		
+		
+			ptr->size = ptr->size + sizeof(Node) + ptr->next->size;
+			
+			if(ptr->next->next != NULL){
+			
+				ptr->next = ptr->next->next;
+				ptr->next->prev = ptr;
+				ptr->allocated = 0;
+			
+			}else{
+			
+				ptr->next = NULL;
+				ptr->allocated = 0;
+				
+			}
+		
+		
+		}else if(ptr->prev->allocated == 0 && ptr->next->allocated != 0){
+		
+			ptr = ptr->prev;
+			ptr->size = ptr->size + sizeof(Node) + ptr->next->size;
+			ptr->next = ptr->next->next;
+			ptr->next->prev = ptr;
+			
+		
+		
+		}else{
+
+			ptr->allocated = 0;
+		
+		}
+	
+	
+	}else if(ptr->next == NULL && ptr->prev == NULL){
+	
+		ptr->allocated = 0;
+	
+	}
+	
+
 }
+
+
+

c4e16b1fd5b945a4a596ea151f91ce6753685954
Akhil Bondlela <akhil.bondlela@gmail.com>
2016-11-07 15:52:43 -0500

    thread practice updates

--- Assignment 2/thread.c
+++ Assignment 2/thread.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 #define NUM_THREADS   5
 
 void *print_hello(void *threadid) {
-  long tid;
-  tid = (long)threadid;
-  printf("Hello World! Thread %ld!\n", tid);
-  pthread_exit(NULL);
+  long tid; //Variable for thrad ID
+  tid = (long)threadid; //Assigns thread ID to tid
+  printf("Hello World! Thread %ld!\n", tid); //Prints Hello World and thread ID
+  pthread_exit(NULL);//Exits the current thread
 }
 
 int main(int argc, char const *argv[]) {
-  pthread_t threads[NUM_THREADS];
-  int rc;
-  long t;
+  pthread_t threads[NUM_THREADS]; //Creates and array of threads of size NUM_THREADS
+  int rc;//initialize return code variable
+  long t;//initialize counter variable for loop
   for(t = 0; t < NUM_THREADS; t++){
-    printf("In main: creating thread %ld\n", t);
-    rc = pthread_create(threads + t, NULL, print_hello, (void *) t);
-    if(rc){
-      printf("ERROR : return code is %d\n", rc);
-      exit(-1);
+    printf("In main: creating thread %ld\n", t);//Prints that it is creating thread with ID
+    rc = pthread_create(threads + t, NULL, print_hello, (void *) t); //Creates thread with driver method
+    if(rc){//Checks for error
+      printf("ERROR : return code is %d\n", rc); //Prints that there is an error with return code
+      exit(-1); //Sends exit signal
     }
   }
-  pthread_exit(NULL);
+  pthread_exit(NULL); //Exits thread
+  return 0;
 }

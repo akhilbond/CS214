29a2470de320b5e474053e93782449c40ad36d58
Akhil Bondlela <akhil.bondlela@gmail.com>
2016-11-07 16:02:59 -0500

    Threads practice

--- Assignment 2/thread.c
+++ Assignment 2/thread.c
@@ -4,24 +4,30 @@
 #define NUM_THREADS   5
 
 void *print_hello(void *threadid) {
-  long tid; //Variable for thrad ID
-  tid = (long)threadid; //Assigns thread ID to tid
-  printf("Hello World! Thread %ld!\n", tid); //Prints Hello World and thread ID
-  pthread_exit(NULL);//Exits the current thread
+        long tid; //Variable for thrad ID
+        tid = (long)threadid; //Assigns thread ID to tid
+        printf("Hello World! Thread %ld!\n", tid); //Prints Hello World and thread ID
+        pthread_exit(NULL);//Exits the current thread
 }
 
 int main(int argc, char const *argv[]) {
-  pthread_t threads[NUM_THREADS]; //Creates and array of threads of size NUM_THREADS
-  int rc;//initialize return code variable
-  long t;//initialize counter variable for loop
-  for(t = 0; t < NUM_THREADS; t++){
-    printf("In main: creating thread %ld\n", t);//Prints that it is creating thread with ID
-    rc = pthread_create(threads + t, NULL, print_hello, (void *) t); //Creates thread with driver method
-    if(rc){//Checks for error
-      printf("ERROR : return code is %d\n", rc); //Prints that there is an error with return code
-      exit(-1); //Sends exit signal
-    }
-  }
-  pthread_exit(NULL); //Exits thread
-  return 0;
+        pthread_t threads[NUM_THREADS]; //Creates and array of threads of size NUM_THREADS
+        int rc;//initialize return code variable
+        long t;//initialize counter variable for loop
+        for(t = 0; t < NUM_THREADS; t++) {
+                printf("In main: creating thread %ld\n", t);//Prints that it is creating thread with ID
+                rc = pthread_create(threads + t, NULL, print_hello, (void *) t); //Creates thread with driver method
+                if(rc) {//Checks for error
+                        printf("ERROR : return code is %d\n", rc); //Prints that there is an error with return code
+                        exit(-1); //Sends exit signal
+                }
+        }
+
+        /*Wait for all threads to complete*/
+        for(t=0; t < NUM_THREADS; t++){
+            pthread_join(threads[t], NULL);
+        }
+
+      pthread_exit(NULL); //Exits thread
+        return 0;
 }

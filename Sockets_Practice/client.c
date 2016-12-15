fee5b2aa316fda763f86f10268246e8edcd836fd
Akhil Bondlela <akhil.bondlela@gmail.com>
2016-12-14 21:41:33 -0500

    Updates

--- Sockets_Practice/client.c
+++ Sockets_Practice/client.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
     struct hostent *server;
 
     char buffer[256];
+    char close[256] = "quit";
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -34,17 +35,32 @@ int main(int argc, char *argv[])
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
+
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
+
     serv_addr.sin_port = htons(portno);
+
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+while(1){
+    //strcpy(buffer,"");
+    memset(buffer,0,sizeof(buffer));
+
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
+
+
+    if(strcmp(buffer,close)-10 == 0){
+      printf("EXIT COMMAND\n");
+      return 0;
+    }
+
     n = write(sockfd,buffer,strlen(buffer));
+
     if (n < 0)
          error("ERROR writing to socket");
     bzero(buffer,256);
@@ -52,5 +68,6 @@ int main(int argc, char *argv[])
     if (n < 0)
          error("ERROR reading from socket");
     printf("%s\n",buffer);
+}
     return 0;
 }
--- Sockets_Practice/server.c
+++ Sockets_Practice/server.c
@@ -38,12 +38,17 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0)
               error("ERROR on binding");
+
+
     while((strcmp(buffer,close))){
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
      if (newsockfd < 0)
           error("ERROR on accept");
+
+      while(1){
+                 memset(buffer,0,sizeof(buffer));
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
      if (n < 0) error("ERROR reading from socket");
@@ -60,5 +65,6 @@ int main(int argc, char *argv[])
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
    }
+ }
      return 0;
 }
